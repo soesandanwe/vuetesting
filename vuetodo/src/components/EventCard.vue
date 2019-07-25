a58ab6254639510224af7518fd5db3d62fa07933
soesandanwe <soesandanwe@gmail.com>
2019-07-25 15:36:33 +0800

    Add Routing

---
+++ vuetodo/src/components/EventCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script>
+export default {
+  name: "EventData",
+  data() {
+    return {
+      quote: "I want to be single"
+    };
+  }
+};
+</script>
+
+<style></style>
